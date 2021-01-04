ecdc1cd99bb2702a7588b15866bb13dc3925f28e
Pavlo <65621560+MNPavlo@users.noreply.github.com>
2021-01-04 18:48:18 +0200

    create form changes

--- meter/forms.py
+++ meter/forms.py
@@ -3,3 +3,5 @@ from django import forms
 
 class CreateMeterForm(forms.Form):
     meter_name = forms.CharField(label='Meter name', max_length=255, min_length=3)
+    resource = forms.CharField(label='Resource', max_length=255, min_length=3)
+    unit = forms.CharField(label='Unit', max_length=255, min_length=3)
